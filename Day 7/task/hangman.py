dd85484bb1ddef2f66643a13ba578a2acfd7416e
Nishchal Bogati <nishchalbogati1@gmail.com>
2026-07-08 15:01:04 +0545

    Added ascii art and lives feature to hangman

--- Day 7/task/hangman.py
+++ Day 7/task/hangman.py
@@ -1,4 +1,61 @@
 import random
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
@@ -12,6 +69,7 @@ print(placeholder)
 
 # TODO-1: - Use a while loop to let the user guess again.
 game_over = False
+lives = 6
 correct_letters = []
 while not game_over:
     guess = input("Guess a letter: ").lower()
@@ -27,6 +85,14 @@ while not game_over:
         else:
                 display += "_"
     print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        print("The letter is not in the word")
+        print(stages[lives])
+        print(f'You have {lives} left.')
+        if lives == 0:
+            game_over = True
+            print("You lose!")
     if "_" not in display:
         game_over = True
         print("You win!")
