1975c951af2a2f37aa18e4d96d168070ac7e9fdd
Nishchal Bogati <nishchalbogati1@gmail.com>
2026-07-21 14:29:32 +0545

    Refactor of Caesar Cipher

--- Day 8/Caesar Cipher 2/CaesarCipher.py
+++ Day 8/Caesar Cipher 2/CaesarCipher.py
@@ -1,33 +1,23 @@
-cipher_text = ""
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    global cipher_text
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
     for letter in original_text:
+
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-def decrypt(encrypted_text, shift_amount):
-    real_text = ""
-    for letter in encrypted_text:
-        shifted_position = alphabet.index(letter) - shift_amount
-        real_text += alphabet[shifted_position % 26]
-    print(f"Here is the decoded result: {real_text}")
 
-if direction == "encode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    encrypt(original_text=text, shift_amount=shift)
-elif direction == "decode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    decrypt(encrypted_text=cipher_text, shift_amount=shift)
-else:
-    print("Please enter 'encode' or 'decode'!")
-
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
 
